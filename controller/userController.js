22b4de86eb9e4f488fe550732dac8d4e189acfda
gautam-patil <gpatil1505@gmail.com>
2023-09-17 18:06:43 +0530

    add mailchimp api

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
+const axios = require('axios');
+
 const UserModel = require('./../models/userModel');
 
 exports.registrationPage = function (req, res) {
 
-        res.render("user/registration");
-    
+    res.render("user/registration");
+
 }
 
 exports.homePage = function (req, res) {
@@ -13,23 +15,51 @@ exports.homePage = function (req, res) {
 
 }
 
-exports.createUser = function (req, res){
+exports.createUser = function (req, res) {
 
     console.log(req.body);
-        
 
-        let crateUserModel = new UserModel();
-          crateUserModel.djName = req.body.djName;
-          crateUserModel.firstName = req.body.firstName;
-          crateUserModel.lastName = req.body.lastName;
-          crateUserModel.email = req.body.email;
-          crateUserModel.contactNumber = req.body.contactNumber;
-          crateUserModel.instagramUsername = req.body.instagramUsername;
 
-          crateUserModel.save((err, doc) => { 
+    let crateUserModel = new UserModel();
+    crateUserModel.djName = req.body.djName;
+    crateUserModel.firstName = req.body.firstName;
+    crateUserModel.lastName = req.body.lastName;
+    crateUserModel.email = req.body.email;
+    crateUserModel.contactNumber = req.body.contactNumber;
+    crateUserModel.instagramUsername = req.body.instagramUsername;
 
-          })
-      
-    
-      res.json({msg:'success'});
+    crateUserModel.save((err, doc) => {
+
+    })
+
+    let data = JSON.stringify({
+        "email_address": req.body.email,
+        "status": "subscribed",
+        "merge_fields": {
+            "FNAME": req.body.firstName,
+            "LNAME": req.body.lastName
+        }
+    });
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://us14.api.mailchimp.com/3.0/lists/e0b5292ee0/members',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic a2V5OjQzMWEyZjgwM2M0MmNjODFhMWJmMzZjYmFjZTBiMDY1LXVzMTQ='
+        },
+        data: data
+    };
+
+    axios.request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+
+    res.json({ msg: 'success' });
 }
